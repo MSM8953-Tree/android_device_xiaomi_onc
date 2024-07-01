LizRelDev/device_xiaomi_onclite

onclite: Add rules to allow cast settings to access WFD props
Signed-off-by: Hadad <repo-sync@outlook.com>
Change-Id: I6015952833c795e364a31ece45306b56cd67cb37

BoardConfig.mk
@@ -182,6 +182,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/configs/resources/releasetools
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
 endif

MSM8953-Tree/android_device_xiaomi_onc

BoardConfig.mk
@@ -186,6 +186,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current